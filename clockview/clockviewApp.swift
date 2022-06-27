146465488c4e3954a456b77241e1e621db3bc9eb
szegedim <szegedim@mav.hu>
2022-06-27 21:44:23 +0200

    Initial Commit

---
+++ clockview/clockviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  clockviewApp.swift
+//  clockview
+//
+//  Created by Szegedi Mihály on 2022. 06. 27..
+//
+
+import SwiftUI
+
+@main
+struct clockviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
